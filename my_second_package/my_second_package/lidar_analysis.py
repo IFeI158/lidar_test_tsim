fc129753ac042c327e1c22ba1564dcc4164d1b67
IFeI158 <samcho1588@gmail.com>
2025-12-01 11:21:24 +0900

    added analysis

---
+++ my_second_package/my_second_package/lidar_analysis.py
@@ -0,0 +1,41 @@
+#Lidar data preprocessing
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+import numpy as np
+
+class LidarAnalyze(Node):
+    def __init__(self):
+        super().__init__('lidar_analyzer')
+        self.subscription = self.create_subscription(
+            LaserScan, "/lidar_data_mock", self.listner_callback, 10)
+    
+    def listner_callback(self, msg):
+        # 1. ROS data to Numpy
+        ranges = np.array(msg.ranges)
+
+        # 2. preprocessing :inf data
+        ranges = np.where(ranges == float('inf'), 3.5, ranges) #inf= 오류값일때는 3.5 inf 가 아닐때는 ranges
+
+        # 3. check front side
+        front_ranges = np.concatenate((ranges[:30], ranges[-30:]))
+
+        # 4. calculate min and mean
+        min_dist = np.min(front_ranges)
+        avg_dist = np.mean(front_ranges)
+
+        safe_dist = 0.3
+        if min_dist < safe_dist:
+            self.get_logger().warn(f"dangerous! {min_dist}m left!") 
+        else :
+            self.get_logger().info(f"safe ranges...{avg_dist}m left")
+
+def main() :
+    rclpy.init()
+    node = LidarAnalyze()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
